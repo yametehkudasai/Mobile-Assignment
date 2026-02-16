91f76737dc761ded7e8cb248e1d4384e0a3f5eb7
yametehkudasai <shumyongsheng@gmail.com>
2026-02-16 09:50:02 +0000

    Feb 16, 2026, 5:50 PM

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -191,4 +191,9 @@ export class HomepagePage implements OnInit {
     });
     await toast.present();
   }
+
+  // go to transaction History
+  goToTransactionHistory() {
+    this.router.navigate(['/transaction-history']);
+  }
 }
